4b65c068a25815efff8888ba45ffa4fd0b1a7a66
Ken T <ken@169-231-219-84.wireless.ucsb.edu>
2025-01-22 15:51:27 -0800

    intergrated with cli task

--- tasks/measurements/ookla_speedtest.py
+++ tasks/measurements/ookla_speedtest.py
@@ -1,51 +1,19 @@
-import typing
 import subprocess
 import json
+from typing import Callable
 
-from netunicorn.base import Architecture, Node, Task, TaskDispatcher
-from netunicorn.library.tasks.tasks_utils import subprocess_run
+from netunicorn.base import Architecture, Failure, Success, Task, TaskDispatcher, Node
+from subprocess import CalledProcessError
+from dataclasses import dataclass
 
+@dataclass
+class SpeedTestOptions:
+    server_id: str = ""
+    server_ip: str = ""
+    timeout: int = 100
 
-class SpeedTest(TaskDispatcher):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.linux_instance = SpeedTestLinuxImplementation(name=self.name)
-
-    def dispatch(self, node: Node) -> Task:
-        if node.architecture in {Architecture.LINUX_AMD64, Architecture.LINUX_ARM64}:
-            return self.linux_instance
-
-        raise NotImplementedError(
-            f"SpeedTest is not implemented for architecture: {node.architecture}"
-        )
-
-
-class SpeedTestLinuxImplementation(Task):
-    requirements = ["pip install speedtest-cli"]
-
-    def run(self):
-        return subprocess_run(["speedtest-cli", "--simple", "--secure"]).map(
-            self._format_data
-        )
-
-    @staticmethod
-    def _format_data(data: str) -> Dict[str, Dict]:
-        ping, download, upload, *other = data.split("\n")
-        return {
-            "ping": {"value": float(ping.split(" ")[1]), "unit": ping.split(" ")[2]},
-            "download": {
-                "value": float(download.split(" ")[1]),
-                "unit": download.split(" ")[2],
-            },
-            "upload": {
-                "value": float(upload.split(" ")[1]),
-                "unit": upload.split(" ")[2],
-            },
-            "other": other,
-        }
-
-
-class Server_Info(typing.TypedDict):
+@dataclass
+class ServerInfo:
     id: int
     host: str
     port: int
@@ -53,16 +21,108 @@ class Server_Info(typing.TypedDict):
     location: str
     country: str
 
-class Server_Selection(Task):
-    def __init__(self, callback: typing.Callable[[list[Server_Info]], int] , *args, **kwargs):
+class OoklaSpeedtest(TaskDispatcher):
+    def __init__(self, options: SpeedTestOptions = SpeedTestOptions(), *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.linux_implementation = OoklaSpeedtestLinuxImplementation(options, name=self.name)
+
+    def dispatch(self, node: Node) -> Task:
+        if node.architecture in {Architecture.LINUX_AMD64, Architecture.LINUX_ARM64}:
+            return self.linux_implementation
+        raise NotImplementedError(
+            f"Ookla_Speedtest_CLI is not implemented for architecture: {node.architecture}"
+        )
+    
+class OoklaSpeedtestLinuxImplementation(Task):
+    requirements = [
+        "apt-get install curl --yes",
+        "curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | bash",
+        "apt-get install speedtest --yes",
+    ]
+
+    def __init__(self, options: SpeedTestOptions, *args, **kwargs):
+        self.timeout = options.timeout
+        self.server_id = options.server_id
+        # self.source_ip = options.source_ip
+        super().__init__(*args, **kwargs)
+    
+    def run(self):
+        try:
+            flags = ["--accept-gdpr", "--accept-license", "--progress=no", "--format=json"]
+
+            if self.server_id != '':
+                flags.append(f"--server-id={self.server_id}")
+            # if self.source_ip != '':
+            #     flags.append(f"--ip={self.source_ip}")
+            
+            result = subprocess.run(["speedtest"] + flags, stdout=subprocess.PIPE)
+            result.check_returncode()
+            return Success({"test_result" : result.stdout})
+                
+        except subprocess.TimeoutExpired:
+            return Failure("Ookla Speedtest timed out.")
+        
+        except CalledProcessError:
+            return Failure(
+                    f"Ookla_Speedtest_CLI failed with return code {result.returncode}. "
+                    f"\nStdout: {result.stdout.strip()} "
+                    f"\nStderr: {result.stderr.strip()}"
+                )
+        
+        except Exception as e:
+            return Failure(f"Exception occurred: {str(e)}")
+        
+
+class ServerSelection(TaskDispatcher):
+    def __init__(self, callback: Callable[[list[ServerInfo]], int], options: SpeedTestOptions = SpeedTestOptions(), *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.linux_instance = ServerSelectionLinuxImplementation(callback, name=self.name)
+
+    def dispatch(self, node: Node) -> Task:
+        if node.architecture in {Architecture.LINUX_AMD64, Architecture.LINUX_ARM64}:
+            return self.linux_instance
+
+        raise NotImplementedError(
+            f"ServerSelection is not implemented for architecture: {node.architecture}"
+        )
+
+class ServerSelectionLinuxImplementation(Task):
+    requirements = [
+        "apt-get install curl --yes",
+        "curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | bash",
+        "apt-get install speedtest --yes",
+    ]
+
+    def __init__(self, callback: Callable[[list[ServerInfo]], int], options: SpeedTestOptions, *args, **kwargs):
         self.callback = callback
         super().__init__(*args, **kwargs)
     
     def run(self):
-        flags = ["--accept-gdpr", "--accept-license", "--progress=no", "--servers", "--format=json"]
-        result = subprocess.run(["speedtest"] + flags, stdout=subprocess.PIPE)
-        if result.returncode == 0:
-            servers = json.loads(result.stdout.decode())["servers"]
+        try:
+            flags = ["--accept-gdpr", "--accept-license", "--progress=no", "--servers", "--format=json"]
+            result = subprocess.run(["speedtest"] + flags, stdout=subprocess.PIPE)
+            result.check_returncode()
+            servers = [
+                ServerInfo(
+                    server["id"],
+                    server["host"],
+                    server["port"],
+                    server["name"],
+                    server["location"],
+                    server["country"]
+                )
+                for server
+                in json.loads(result.stdout.decode())["servers"]
+            ]
             return {"server_id": self.callback(servers)}
-        else:
-            return Failure("Failed somehow") # todo: fix error msg
+        
+        except CalledProcessError:
+            return Failure(
+                f"Ookla_Speedtest_CLI failed with return code {result.returncode}. "
+                f"\nStdout: {result.stdout.strip()} "
+                f"\nStderr: {result.stderr.strip()}"
+            )
+        
+if __name__ == "__main__":
+    cli_task = OoklaSpeedtest(name="Ookla CLI Speedtest")
+    print(cli_task.run())
